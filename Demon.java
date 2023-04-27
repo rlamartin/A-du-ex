0a05b2352622cae76e08d2fd5ed5ac60b043c138
rlamartin <92861184+rlamartin@users.noreply.github.com>
2023-04-27 11:20:09 +0800

    Update Demon.java

--- Demon.java
+++ Demon.java
@@ -1,35 +1,38 @@
 package dungeonexca;
 
-public abstract class Demon{
-    protected String demonType, hierarchy;
-    protected int demonHp, demonDamage;
-    protected Utility heldItem;
-
-    public Demon(String dT, String h, int dH, int dD) {
-        demonType = dT;
-        hierarchy = h;
-        demonHp = dH;
-        demonDamage = dD;
+public abstract class Demon {
+    private String demonType, hierarchy;
+    private int demonMaxHp, demoncurrentHp, demonDamage, demoncriticalHit, demonSpeed;
+    private double demonCDamage;
+    private Utility heldItem;
+    
+    public Demon(String n){
+        demonType = n;
+        
     }
-
     public String getType() {
         return demonType;
     }
-
     public String getHierarchy() {
         return hierarchy;
     }
-
-    public int getDemonHp() {
-        return demonHp;
+    public int getdemoncurrentHp() {
+        return demoncurrentHp;
     }
-
-    public int getDemonDamage() {
+    public int getdemonMaxhp() {
+        return demonMaxHp;
+    }
+    public int getdemonDamage() {
         return demonDamage;
     }
-    
+    public int getdemonSpeed() {
+        return demonSpeed;
+    }
+    public int getdemoncriticalHit() {
+        return demoncriticalHit;
+    }
     public void attack(Player p){
-        p.currentHp -= demonDamage;
+        p.getExcacurrentHp() -= demonDamage;
     }
     public void equip(Utility u){
         heldItem = u;
